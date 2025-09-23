79c486eb0db23cf199294cb4f0d7d3bfcee4e76d
Hisuuz06 <24021474@vnu.edu.vn>
2025-09-23 23:39:58 +0700

    B HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Lê Trung Hiếu\t24021474\tK69I-CS5\tHisuuz06\t24021474@vnu.edu.vn");
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
