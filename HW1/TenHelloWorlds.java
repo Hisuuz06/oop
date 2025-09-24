5e506bd6d47caf86e3ff23f3e38642a022eab002
Hisuuz06 <24021474@vnu.edu.vn>
2025-09-24 09:02:11 +0700

    format

--- HW1/TenHelloWorlds.java
+++ HW1/TenHelloWorlds.java
@@ -1,7 +1,7 @@
 public class TenHelloWorlds {
-    public static void main(String[] args){
-        int i=1;
-        while(i<=10){
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
             System.out.println("Hello, World");
             i++;
         }
