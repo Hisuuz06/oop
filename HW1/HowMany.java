d59643c036f96b54c611cb419d837945b55073d3
Hisuuz06 <24021474@vnu.edu.vn>
2025-09-24 13:17:56 +0700

    1.4.11

---
+++ HW1/HowMany.java
@@ -0,0 +1,6 @@
+public class HowMany {
+    public static void main(String[] args){
+        int size = args.length;
+        System.out.println("You entered " + size + " command-line argument");
+    }
+}
