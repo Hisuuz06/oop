d19f41cab91c273a4d12feda8544266eb8098a05
Hisuuz06 <24021474@vnu.edu.vn>
2025-09-30 23:48:28 +0700

    HW2 TetrisGrid

--- HW2/src/main/java/TetrisGrid.java
+++ HW2/src/main/java/TetrisGrid.java
@@ -3,27 +3,48 @@
 // a clearRows() capability.
 
 public class TetrisGrid {
-	
-	/**
-	 * Constructs a new instance with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
-	public TetrisGrid(boolean[][] grid) {
-	}
-	
-	
-	/**
-	 * Does row-clearing on the grid (see handout).
-	 */
-	public void clearRows() {
-	}
-	
-	/**
-	 * Returns the internal 2d grid array.
-	 * @return 2d grid array
-	 */
-	boolean[][] getGrid() {
-		return null; // YOUR CODE HERE
-	}
+    private boolean grid[][];
+
+    /**
+     * Constructs a new instance with the given grid.
+     * Does not make a copy.
+     *
+     * @param grid
+     */
+    public TetrisGrid(boolean[][] grid) {
+        this.grid = grid;
+    }
+
+
+    /**
+     * Does row-clearing on the grid (see handout).
+     */
+    public void clearRows() {
+        for (int j = 0; j < grid[0].length; j++) {
+            boolean check = true;
+            for (int i = 0; i < grid.length; i++) {
+                if (grid[i][j] == false) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                for (int i = 0; i < grid.length; i++) {
+                    for (int k = j; k + 1 < grid[0].length; k++) {
+                        grid[i][k] = grid[i][k + 1];
+                    }
+                    grid[i][grid[0].length - 1] = false;
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the internal 2d grid array.
+     *
+     * @return 2d grid array
+     */
+    boolean[][] getGrid() {
+        return grid; // YOUR CODE HERE
+    }
 }
